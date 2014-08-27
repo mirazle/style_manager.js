70a1e7f9d411819f0c762dc71e3d3d24263d23f6
mirazle <mirazle2069@gmail.com>
2014-08-27 16:35:55 +0900

    Update style_manager.js

--- style_manager.js
+++ style_manager.js
@@ -5,9 +5,7 @@ $(function() {
     // Define Base Function
     var style_manager = new Function;
 
-    var inner_css_no = 1;
-
-    function get_file_name(styles_num) {
+    function get_css_name(styles_num) {
 
         if (document.styleSheets[styles_num]['ownerNode']['dataset']['href'] != undefined) {
 
@@ -30,15 +28,11 @@ $(function() {
 
         for (var styleSheetsNum = 0; styleSheetsNum < document.styleSheets.length; styleSheetsNum++) {
 
-            var file_name = get_file_name(styleSheetsNum);
-
-            map[file_name] = {};
-
             var rules = (document.styleSheets[styleSheetsNum]['rules'] != undefined) ?
                 document.styleSheets[styleSheetsNum]['rules'] :
                 document.styleSheets[styleSheetsNum]['cssRules'];
 
-            set_rules(file_name, rules, styleSheetsNum);
+            set_rules(rules, styleSheetsNum);
 
             if (styleSheetsNum == document.styleSheets.length - 1) {
 
@@ -54,7 +48,7 @@ $(function() {
         }
     });
 
-    function set_rules(file_name, rules, styleSheetsNum, rec_flg) {
+    function set_rules(rules, styleSheetsNum, rec_flg) {
 
         var rec_flg = (rec_flg == undefined) ? false : rec_flg;
 
@@ -68,95 +62,96 @@ $(function() {
                     rules[style_num]['parentRule']['media']['mediaText'] : 'all';
             }
 
-            if (map[file_name][media] == undefined) map[file_name][media] = {};
+            if (map[media] == undefined) map[media] = {};
 
             switch (rules[style_num]['type']) {
                 case 3:
-                    set_import(file_name, rules, media, style_num);
+                    set_import(rules, media, style_num);
                     break;
                 case 4:
+
                     var dig_key = (rules[style_num]['rules'] != undefined) ? 'rules' : 'cssRules';
-                    set_rules(file_name, rules[style_num][dig_key], styleSheetsNum, true);
+                    set_rules(rules[style_num][dig_key], styleSheetsNum, true);
                     break;
                 case 5:
-                    set_fontface(file_name, rules, media, style_num);
+                    set_fontface(rules, media, style_num);
                     break;
                 case 6:
-                    set_page(file_name, rules, media, style_num);
+                    set_page(rules, media, style_num);
                     break;
                 case 7:
-                    set_keyframe(file_name, rules, media, style_num);
+                    set_keyframe(rules, media, style_num);
                     break;
                 default:
-                    set_style(file_name, rules, media, style_num, rules[style_num]['type']);
+                    set_style(rules, media, style_num, rules[style_num]['type']);
                     break;
             }
         }
     }
 
-    function set_page(file_name, rules, media, style_num, type) {
+    function set_page(rules, media, style_num, type) {
 
-        if (map[file_name][media]['@page'] == undefined) {
-            map[file_name][media]['@page'] = {};
+        if (map[media]['@page'] == undefined) {
+            map[media]['@page'] = {};
         }
 
         for (var i = 0; i < rules[style_num]['style'].length; i++) {
 
-            map[file_name][media]['@page'][rules[style_num]['style'][i]] =
+            map[media]['@page'][rules[style_num]['style'][i]] =
                 rules[style_num]['style'][rules[style_num]['style'][i]];
         }
     }
 
-    function set_import(file_name, rules, media, style_num) {
+    function set_import(rules, media, style_num) {
 
-        if (map[file_name][media]['@import'] == undefined) {
-            map[file_name][media]['@import'] = new Array();
+        if (map[media]['@import'] == undefined) {
+            map[media]['@import'] = new Array();
         }
 
-        map[file_name][media]['@import'].push(rules[style_num]['cssText'].replace('@import ', ''));
+        map[media]['@import'].push(rules[style_num]['cssText'].replace('@import ', ''));
     }
 
-    function set_style(file_name, rules, media, style_num) {
+    function set_style(rules, media, style_num) {
 
         for (var i = 0; i < rules[style_num]['style'].length; i++) {
 
-            if (map[file_name][media][rules[style_num]['selectorText']] == undefined) {
+            if (map[media][rules[style_num]['selectorText']] == undefined) {
 
-                map[file_name][media][rules[style_num]['selectorText']] = {};
+                map[media][rules[style_num]['selectorText']] = {};
             }
 
-            map[file_name][media][rules[style_num]['selectorText']][rules[style_num]['style'][i]] =
+            map[media][rules[style_num]['selectorText']][rules[style_num]['style'][i]] =
                 rules[style_num]['style'][rules[style_num]['style'][i]];
         }
     }
 
-    function set_fontface(file_name, rules, media, style_num) {
+    function set_fontface(rules, media, style_num) {
 
         var font_face = {};
 
-        if (map[file_name][media]['@font-face'] == undefined) {
-            map[file_name][media]['@font-face'] = new Array();
+        if (map[media]['@font-face'] == undefined) {
+            map[media]['@font-face'] = new Array();
         }
 
         for (var i = 0; i < rules[style_num]['style'].length; i++) {
             font_face[rules[style_num]['style'][i]] = rules[style_num]['style'][rules[style_num]['style'][i]];
         }
 
-        map[file_name][media]['@font-face'].push(font_face);
+        map[media]['@font-face'].push(font_face);
     }
 
-    function set_keyframe(file_name, rules, media, style_num) {
+    function set_keyframe(rules, media, style_num) {
 
         var key_name = rules[style_num]['cssText'].split(rules[style_num]['name'])[0].replace(' ', '');
 
-        if (map[file_name][media][key_name] == undefined) {
+        if (map[media][key_name] == undefined) {
 
-            map[file_name][media][key_name] = {};
+            map[media][key_name] = {};
         }
 
-        if (map[file_name][media][key_name][rules[style_num]['name']] == undefined) {
+        if (map[media][key_name][rules[style_num]['name']] == undefined) {
 
-            map[file_name][media][key_name][rules[style_num]['name']] = {};
+            map[media][key_name][rules[style_num]['name']] = {};
         }
 
         var dig_key = (rules[style_num]['rules'] != undefined) ? 'rules' : 'cssRules';
@@ -164,19 +159,19 @@ $(function() {
 
         for (var i = 0; i < rules[style_num][dig_key].length; i++) {
 
-            if (map[file_name][media][key_name][rules[style_num]['name']] == undefined) {
+            if (map[media][key_name][rules[style_num]['name']] == undefined) {
 
-                map[file_name][media][key_name][rules[style_num]['name']] = {};
+                map[media][key_name][rules[style_num]['name']] = {};
             }
 
             for (var j = 0; j < rules[style_num][dig_key][i]['style'].length; j++) {
 
-                if (map[file_name][media][key_name][rules[style_num]['name']][rules[style_num][dig_key][i]['keyText']] == undefined) {
+                if (map[media][key_name][rules[style_num]['name']][rules[style_num][dig_key][i]['keyText']] == undefined) {
 
-                    map[file_name][media][key_name][rules[style_num]['name']][rules[style_num][dig_key][i]['keyText']] = {}
+                    map[media][key_name][rules[style_num]['name']][rules[style_num][dig_key][i]['keyText']] = {}
                 }
 
-                map[file_name][media][key_name][rules[style_num]['name']][rules[style_num][dig_key][i]['keyText']][rules[style_num][dig_key][i]['style'][j]] =
+                map[media][key_name][rules[style_num]['name']][rules[style_num][dig_key][i]['keyText']][rules[style_num][dig_key][i]['style'][j]] =
                     rules[style_num][dig_key][i]['style'][rules[style_num][dig_key][i]['style'][j]];
             }
         }
